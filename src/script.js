ada74d5803119ec6a08540b5cec426abdde912cc
Wajid Ali <127591211+WajidAli05@users.noreply.github.com>
2024-01-15 23:47:25 +0500

    Added load more news and fetch news when html loads

--- src/script.js
+++ src/script.js
@@ -1,26 +1,33 @@
+
+//first 10 news from page 1 are fetched as the html loads completely
+document.addEventListener("DOMContentLoaded" , ()=>{
+    fetchNews(1);
+})
+
+let str = ``;
 async function fetchNews(pageNumber){
     var url = 'https://newsapi.org/v2/everything?' +
-          'q=cricket top news&' +
+          'q=cricket news&' +
           'from=2024-01-11&' +
           'pageSize=10&'+
           'page='+pageNumber +
           'sortBy=popularity&' +
-          'apiKey=94c9a197233c4e1baa1e77c02ac338d4';
+          'apiKey=YOUR_API_KEY_GOES_HERE';
 
 var req = new Request(url);
 let jsonResponse = await fetch(req);
 let response = await jsonResponse.json();
 console.log(response);
-let str = ``;
+
 
 for(let article of response.articles){
-    str = str + `<div class="card">
+    str = str + `<div class="news_card">
     <img src="${article.urlToImage}" />
     <h4>${article.title}</h4>
     <p>${article.description}
     </p>
     <small><strong>Published On: </strong>${article.publishedAt}</small>
-    <button><a href="${article.url}" target = "_blank">Read More!</a></button>
+    <button><a href="${article.url}" target = "_blank">Read More</a></button>
     </div>`
 }
 
