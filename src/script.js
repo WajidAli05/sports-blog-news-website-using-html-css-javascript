7c6a6a6c8fc34bc1d1bc269255950cc732102bfc
Wajid Ali <127591211+WajidAli05@users.noreply.github.com>
2024-01-17 13:58:50 +0500

    Remove hard coded values from api calling

--- src/script.js
+++ src/script.js
@@ -1,18 +1,20 @@
-
+let newsPageNumber = 1;
+let newsNumber = 10;
 //first 10 news from page 1 are fetched as the html loads completely
 document.addEventListener("DOMContentLoaded" , ()=>{
-    fetchNews(1);
+    fetchNews(newsPageNumber , newsNumber);
 })
 
 let str = ``;
-async function fetchNews(pageNumber){
+async function fetchNews(newsPageNumber , newsNumber){
+    let currentDate = new Date();
     var url = 'https://newsapi.org/v2/everything?' +
           'q=cricket news&' +
-          'from=2024-01-11&' +
-          'pageSize=10&'+
-          'page='+pageNumber +
+          `from=${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDate() - 2}&` +
+          `pageSize=${newsNumber}&`+
+          `page=${newsPageNumber}`+ // this is actually the page number the api will bring news from
           'sortBy=popularity&' +
-          'apiKey=YOUR_API_KEY_GOES_HERE';
+          'apiKey=94c9a197233c4e1baa1e77c02ac338d4';
 
 var req = new Request(url);
 let jsonResponse = await fetch(req);
@@ -33,5 +35,13 @@ for(let article of response.articles){
 
 document.querySelector(".all-news").innerHTML = str;
 
+//fetch news from the same page until all the news are fetched. Once this page ends, then move to other pages
+if(newsPageNumber <= response.totalResults){
+    newsPageNumber++;
+    newsNumber = 10;
+}
+else{
+    newsNumber = newsNumber + 10;
+}
 }
 
